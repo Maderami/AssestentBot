b2bde847c04b3a9ec652f3085cabba5b1bcb72f9
Madermi <sdsoerty6584@gmail.com>
2022-03-19 18:31:45 +0600

    sadas

---
+++ AssistantBotForHackathon/Repos/HistoryRepo.cs
@@ -0,0 +1,13 @@
+﻿using AssistantBotForHackathon.Entities;
+using System.Collections.Generic;
+
+namespace AssistantBotForHackathon.Repos
+{
+    public static class HistoryRepo
+    {
+        private static List<History> _historyList = new List<History>();
+        public static List<History> GetHistoryList() => _historyList;
+
+        public static void Add(History history) => _historyList.Add(history);
+    }
+}
